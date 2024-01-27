61a586f7bbd01f4d41d0dbde3acd1f948e9a5e28
octocatblain <blainmuema@gmail.com>
2024-01-27 17:01:55 +0300

    fix: correct navlink active link js

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,19 +1,19 @@
 // NAVLINKS ACTIVE CLASS
 
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+let section = document.querySelectorAll('section');
+let navLink = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
-    sections.forEach(sec => {
+    section.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop;
         let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+        let id = sec.getAttribute('id')
 
         if (top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a [href*=' + id + ']').classList.add('active');
+            navLink.forEach(links => {
+                links.classList.remove('active')
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
             });
         };
     });
