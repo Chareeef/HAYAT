09764cb95c220db3b0e3904a7c76b019c8a3e049
octocatblain <blainmuema@gmail.com>
2024-01-29 14:26:57 +0300

    feat: add back to top button

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -33,6 +33,30 @@ toggleButton.onclick = function () {
 };
 
 
+// BACK TO TOP BUTTON
+
+var backToTopButton = document.getElementById("backToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    backToTopButton.style.display = "block";
+  } else {
+    backToTopButton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+backToTopButton.onclick = function() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+};
+
+
 
 
 
