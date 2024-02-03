55a0b857259ff7cb5d68019306f5196120de8ca3
Youssef Charif Hamidi <youssef.charif.h@gmail.com>
2024-02-03 17:15:26 +0000

    Enhance test_storage.py file

--- tests/test_db/test_storage.py
+++ tests/test_db/test_storage.py
@@ -40,6 +40,42 @@ class TestStorageInstance(unittest.TestCase):
         self.assertEqual(self.storage.all(), [])
 
 
+def fill_database(self):
+    """Return a ready test storage"""
+    storage = Storage()
+    storage.load_all()
+
+    self.mor = Country(name='Morrocco')
+    self.eth = Country(name='Ethiopia')
+    storage.add_all([self.mor, self.eth])
+    storage.commit()
+
+    self.err = City(name='Errachidia', country_id=self.mor.id)
+    storage.add(self.err)
+    storage.commit()
+
+    self.sah = TC(name='Sahraoui',
+                  email='sah@ex.com', password_hash='nkmjuh',
+                  phone_number='0334129876', city_id=self.err.id)
+    storage.add(self.sah)
+    storage.commit()
+
+    self.ych = Donor(username='youcha',
+                     email='ych@ex.com', password_hash='poulkll',
+                     phone_number='0607798008',
+                     full_name='Youssef Charif', age=21,
+                     gender='Male', blood_category='O-')
+    storage.add(self.ych)
+    storage.commit()
+
+    self.bag = BloodBag(blood_category='O-', quantity=6, situation='Critic',
+                        center_id=self.sah.id)
+    storage.add(self.bag)
+    storage.commit()
+
+    self.storage = storage
+
+
 class TestStorageAll(unittest.TestCase):
     """Test Storage all() method"""
 
@@ -48,38 +84,7 @@ class TestStorageAll(unittest.TestCase):
         """Runs at class beginning to create tables,
         sqlalchemy session and some sample data
         """
-        storage = Storage()
-        storage.load_all()
-
-        cls.mor = Country(name='Morrocco')
-        cls.chili = Country(name='Chili')
-        storage.add_all([cls.mor, cls.chili])
-        storage.commit()
-
-        cls.err = City(name='Errachidia', country_id=cls.mor.id)
-        storage.add(cls.err)
-        storage.commit()
-
-        cls.sah = TC(name='Sahraoui',
-                     email='sah@ex.com', password_hash='nkmjuh',
-                     phone_number='0334129876', city_id=cls.err.id)
-        storage.add(cls.sah)
-        storage.commit()
-
-        cls.ych = Donor(username='youcha',
-                        email='ych@ex.com', password_hash='poulkll',
-                        phone_number='0607798008',
-                        full_name='Youssef Charif', age=21,
-                        gender='Male', blood_category='O-')
-        storage.add(cls.ych)
-        storage.commit()
-
-        cls.bag = BloodBag(blood_category='O-', quantity=6, situation='Critic',
-                           center_id=cls.sah.id)
-        storage.add(cls.bag)
-        storage.commit()
-
-        cls.storage = storage
+        fill_database(cls)
 
     @classmethod
     def tearDownClass(cls):
@@ -92,7 +97,7 @@ class TestStorageAll(unittest.TestCase):
 
         self.assertEqual(len(all_dict), 6)
         self.assertIn(self.mor, all_dict)
-        self.assertIn(self.chili, all_dict)
+        self.assertIn(self.eth, all_dict)
         self.assertIn(self.err, all_dict)
         self.assertIn(self.ych, all_dict)
         self.assertIn(self.sah, all_dict)
@@ -104,7 +109,7 @@ class TestStorageAll(unittest.TestCase):
 
         self.assertEqual(len(country_dict), 2)
         self.assertIn(self.mor, country_dict)
-        self.assertIn(self.chili, country_dict)
+        self.assertIn(self.eth, country_dict)
         self.assertNotIn(self.err, country_dict)
         self.assertNotIn(self.ych, country_dict)
         self.assertNotIn(self.sah, country_dict)
@@ -116,7 +121,7 @@ class TestStorageAll(unittest.TestCase):
 
         self.assertEqual(len(city_dict), 1)
         self.assertNotIn(self.mor, city_dict)
-        self.assertNotIn(self.chili, city_dict)
+        self.assertNotIn(self.eth, city_dict)
         self.assertIn(self.err, city_dict)
         self.assertNotIn(self.ych, city_dict)
         self.assertNotIn(self.sah, city_dict)
@@ -128,7 +133,7 @@ class TestStorageAll(unittest.TestCase):
 
         self.assertEqual(len(donor_dict), 1)
         self.assertNotIn(self.mor, donor_dict)
-        self.assertNotIn(self.chili, donor_dict)
+        self.assertNotIn(self.eth, donor_dict)
         self.assertNotIn(self.err, donor_dict)
         self.assertIn(self.ych, donor_dict)
         self.assertNotIn(self.sah, donor_dict)
@@ -140,7 +145,7 @@ class TestStorageAll(unittest.TestCase):
 
         self.assertEqual(len(center_dict), 1)
         self.assertNotIn(self.mor, center_dict)
-        self.assertNotIn(self.chili, center_dict)
+        self.assertNotIn(self.eth, center_dict)
         self.assertNotIn(self.err, center_dict)
         self.assertNotIn(self.ych, center_dict)
         self.assertIn(self.sah, center_dict)
@@ -157,7 +162,7 @@ class TestStorageAll(unittest.TestCase):
 
         self.assertEqual(len(bag_dict), 1)
         self.assertNotIn(self.mor, bag_dict)
-        self.assertNotIn(self.chili, bag_dict)
+        self.assertNotIn(self.eth, bag_dict)
         self.assertNotIn(self.err, bag_dict)
         self.assertNotIn(self.ych, bag_dict)
         self.assertNotIn(self.sah, bag_dict)
@@ -171,25 +176,7 @@ class TestStorageAddCommit(unittest.TestCase):
         """Runs before each test to create tables,
         sqlalchemy session and some sample data
         """
-        storage = Storage()
-        storage.load_all()
-
-        self.mor = Country(name='Morrocco')
-        self.eth = Country(name='Ethiopia')
-        storage.add_all([self.mor, self.eth])
-        storage.commit()
-
-        self.err = City(name='Errachidia', country_id=self.mor.id)
-        storage.add(self.err)
-        storage.commit()
-
-        self.sah = TC(name='Sahraoui',
-                      email='sah@ex.com', password_hash='nkmjuh',
-                      phone_number='0334129876', city_id=self.err.id)
-        storage.add(self.sah)
-        storage.commit()
-
-        self.storage = storage
+        fill_database(self)
 
     def tearDown(self):
         """Runs after each test to close the sqlalchemy session"""
@@ -223,20 +210,20 @@ class TestStorageAddCommit(unittest.TestCase):
 
     def test_add_donor(self):
         """Test adding a donor to the database"""
-        ych = Donor(username='youcha',
-                    email='ych@ex.com', password_hash='poulkll',
-                    phone_number='0607798008',
-                    full_name='Youssef Charif', age=21,
-                    gender='Male', blood_category='O-')
+        ken = Donor(username='KencKde',
+                    email='ken@ex.com', password_hash='mjloghl',
+                    phone_number='0987634599',
+                    full_name='Ken', age=21,
+                    gender='Male', blood_category='A+')
 
-        self.assertNotIn(ych, self.storage.all())
-        self.assertNotIn(ych, self.storage.all('Donor'))
+        self.assertNotIn(ken, self.storage.all())
+        self.assertNotIn(ken, self.storage.all('Donor'))
 
-        self.storage.add(ych)
+        self.storage.add(ken)
         self.storage.commit()
 
-        self.assertIn(ych, self.storage.all())
-        self.assertIn(ych, self.storage.all('Donor'))
+        self.assertIn(ken, self.storage.all())
+        self.assertIn(ken, self.storage.all('Donor'))
 
     def test_add_transfusion_center(self):
         """Test adding a transfusion_center to the database"""
@@ -257,7 +244,7 @@ class TestStorageAddCommit(unittest.TestCase):
 
     def test_add_blood_bag(self):
         """Test adding a blood_bag to the database"""
-        bag = BloodBag(blood_category='O-', quantity=6, situation='Critic',
+        bag = BloodBag(blood_category='AB-', quantity=20, situation='Stable',
                        center_id=self.sah.id)
 
         self.assertNotIn(bag, self.storage.all())
@@ -273,11 +260,11 @@ class TestStorageAddCommit(unittest.TestCase):
         """Test adding multiple entries at once with add_all()"""
         ken = Country(name='Kenya')
         mek = City(name='Mekele', country_id=self.eth.id)
-        ych = Donor(username='youcha',
-                    email='ych@ex.com', password_hash='poulkll',
-                    phone_number='0607798008',
-                    full_name='Youssef Charif', age=21,
-                    gender='Male', blood_category='O-')
+        bln = Donor(username='blainoctocat',
+                    email='bln@ex.com', password_hash='frtujh78',
+                    phone_number='0812354760',
+                    full_name='Blain', age=25,
+                    gender='Male', blood_category='AB+')
         mol = TC(name='Moulay Ali Chrif',
                  email='mol@ex.com', password_hash='holatrop',
                  phone_number='0194129876', city_id=self.err.id)
@@ -286,16 +273,16 @@ class TestStorageAddCommit(unittest.TestCase):
 
         self.assertNotIn(ken, self.storage.all())
         self.assertNotIn(mek, self.storage.all())
-        self.assertNotIn(ych, self.storage.all())
+        self.assertNotIn(bln, self.storage.all())
         self.assertNotIn(mol, self.storage.all())
         self.assertNotIn(bag, self.storage.all())
 
-        self.storage.add_all([ken, mek, ych, mol, bag])
+        self.storage.add_all([ken, mek, bln, mol, bag])
         self.storage.commit()
 
         self.assertIn(ken, self.storage.all())
         self.assertIn(mek, self.storage.all())
-        self.assertIn(ych, self.storage.all())
+        self.assertIn(bln, self.storage.all())
         self.assertIn(mol, self.storage.all())
         self.assertIn(bag, self.storage.all())
 
@@ -307,38 +294,7 @@ class TestStorageDelete(unittest.TestCase):
         """Runs before each test to create tables,
         sqlalchemy session and some sample data
         """
-        storage = Storage()
-        storage.load_all()
-
-        self.mor = Country(name='Morrocco')
-        self.chili = Country(name='Chili')
-        storage.add_all([self.mor, self.chili])
-        storage.commit()
-
-        self.err = City(name='Errachidia', country_id=self.mor.id)
-        storage.add(self.err)
-        storage.commit()
-
-        self.sah = TC(name='Sahraoui',
-                      email='sah@ex.com', password_hash='nkmjuh',
-                      phone_number='0334129876', city_id=self.err.id)
-        storage.add(self.sah)
-        storage.commit()
-
-        self.ych = Donor(username='youcha',
-                         email='ych@ex.com', password_hash='poulkll',
-                         phone_number='0607798008',
-                         full_name='Youssef Charif', age=21,
-                         gender='Male', blood_category='O-')
-        storage.add(self.ych)
-        storage.commit()
-
-        self.bag = BloodBag(blood_category='O-', quantity=6, situation='Critic',
-                            center_id=self.sah.id)
-        storage.add(self.bag)
-        storage.commit()
-
-        self.storage = storage
+        fill_database(self)
 
     def tearDown(self):
         """Runs after each test to close the sqlalchemy session"""
