5bb7c3ba8e781c934023128643cf4aae04e0d77c
Youssef Charif Hamidi <youssef.charif.h@gmail.com>
2024-02-04 13:45:16 +0000

    Add flask app home route

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""Module containing our Flask app"""
+from flask import Flask, render_template
+# from routes.[...] import *
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+@app.route('/home/', strict_slashes=False)
+def home():
+    """Home route"""
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
