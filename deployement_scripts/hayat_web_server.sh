4ad73044ede7d967b8179535c4a1f0d82ad3f177
Youssef Charif Hamidi <youssef.charif.h@gmail.com>
2024-02-04 15:57:44 +0000

    Add deployement_scripts/hayat_web_server.sh to set nginx web server

---
+++ deployement_scripts/hayat_web_server.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# Deploy HAYAT website through nginx web server
+
+last_update=$(stat -c %Y /var/lib/apt/periodic/update-success-stamp 2>/dev/null || stat -c %Y /var/cache/apt/pkgcache.bin 2>/dev/null)
+
+current_time=$(date +%s)
+update_threshold=$((24 * 60 * 60))  # 24 hours in seconds
+
+if [ "$((current_time - last_update))" -gt "$update_threshold" ]; then
+    sudo apt update
+fi
+
+if ! dpkg -l | grep -q nginx; then
+    sudo apt install nginx -y
+fi
+
+printf "# Server block for HAYAT website
+
+server {
+	listen 80 default_server;
+	listen [::]:80 default_server;
+
+	server_name _;
+
+	add_header ServedBy %s;
+
+	location / {
+		proxy_pass http://localhost:5000;
+	}
+}
+" "$(hostname)" | sudo tee /etc/nginx/sites-available/default >/dev/null
+
+sudo ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/
+
+sudo systemctl restart nginx
