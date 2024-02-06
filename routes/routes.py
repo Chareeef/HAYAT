d87645362e2c9a9d0cadcf01efd2ec6548392f97
Youssef Charif Hamidi <youssef.charif.h@gmail.com>
2024-02-06 13:59:30 +0000

    Querying data

--- routes/routes.py
+++ routes/routes.py
@@ -2,8 +2,8 @@
 """
 Our Project Flask Routes
 """
-from flask import render_template, request
-from flask_login import LoginManager, login_required, login_user
+from flask import render_template, redirect, request, url_for
+from flask_login import current_user, LoginManager, login_required, login_user, logout
 import secrets
 from db import storage
 from api.maps.donor import *
@@ -20,55 +20,84 @@ login_manager = LoginManager(app)
 @app.route('/', strict_slashes=False)
 @app.route('/home', strict_slashes=False)
 def home_page():
-    """provide Home page"""
-    return "Home Page"
+    """Render Home page"""
+    return render_template('index.html')
 
 
-@app.route('/login', methods=['GET', 'POST'], strict_slashes=False)
+@app.route('/login', strict_slashes=False)
+def login_page():
+    """Render Login page for both Donor and Transfusion Center"""
+    return render_template('login.html')
+
+
+@app.route('/login_donor', methods=['POST'], strict_slashes=False)
 def login_donor():
     """Render login page"""
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password_hash')
-        
-        donor = None # here query a donor with the above username
-        
-        if donor and donor.password_hash == password:
-            login_user(donor, remember=True)
-            return # redirect to dashboard
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
+    username = request.form.get('username')
+    password = request.form.get('password')
+    
+    donors = storage.all('Donor')
+
+    donor = None
+    for d in donors:
+        if d.username == username and bcrypt.check_password_hash(c.password_hash, # TODO
+                                                                 password):
+    
+    if donor:
+        login_user(donor, remember=True)
+        return redirect(url_for('donor_dashboard'))
+    else:
+        # flash error
+        return render_template('login.html')
 
 
 @app.route('/login_center', methods=['POST'], strict_slashes=False)
 def login_center():
     """Login as Transfusion Center"""
-    if request.method == 'POST':
-        email = request.form.get('email')
-        password = request.form.get('password_hash')
-        
-        center = None # here, query a center with the above username
-        
-        if center and center.password_hash == password:
-            login_user(center, remember=True)
-            return # redirect to dashboard
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+    
+    centers = storage.all('TransfusionCenter')
+
+    center = None
+    for c in centers:
+        if c.email == email and bcrypt.check_password_hash(c.password_hash, # TODO
+                                                           password):
+            center = c
+    
+    if center:
+        login_user(center, remember=True)
+        return redirect(url_for('center_dashboard'))
+    else:
+        # flash error
+        return render_template('login.html')
 
 
 @app.route('/register_center', methods=['POST'], strict_slashes=False)
 def register_center():
     """Register New Transfusion Center"""
-    Register_transfusion_center(request.get_json())
+    # Create via api
+    pass
 
 
 @app.route('/register_donor', methods=['POST'], strict_slashes=False)
 def register_donor():
     """Register New Donor"""
-    create_donor(request.get_json())
+    # Create via api
+    pass
 
 
 @app.route('/center_dashboard', strict_slashes=False)
 @login_required
 def center_dashboard():
     """Render Transfusion Center dashboard"""
-    pass  # call to api for data exchange.
+    pass
 
 
 @app.route('/donor_dashboard', strict_slashes=False)
@@ -82,5 +111,5 @@ def donor_dashboard():
 @login_required
 def logout():
     """ Logout """
-    pass # redirect to home page.
-
+    login_user()
+    return redirect(url_for('home'))
