64022f2b4825834b0a4ec5e1b110e7b1c807420c
Youssef Charif Hamidi <youssef.charif.h@gmail.com>
2024-02-08 22:51:44 +0000

    Fix secret key

--- app.py
+++ app.py
@@ -6,11 +6,10 @@ from db.models.transfusion_center import TransfusionCenter as TC
 from flask import Flask
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
-import secrets
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = secrets.token_hex(24)
+app.config['SECRET_KEY'] = '9c714e2657ea33a5d8fed340b5f10e5cbb5d0393e2'
 bcrypt = Bcrypt(app)
 
 login_manager = LoginManager(app)
