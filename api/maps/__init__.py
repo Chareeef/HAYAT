95d879722fbc5698cd9ff587bed5be6b698a666f
Youssef Charif Hamidi <youssef.charif.h@gmail.com>
2024-02-08 13:23:47 +0000

    Fix api import

--- api/maps/__init__.py
+++ api/maps/__init__.py
@@ -1,8 +1,9 @@
+from flask import Blueprint
+
+blood_map = Blueprint('blood_map', __name__, url_prefix='/api')
+
 from api.maps.transfusion_center import *
 from api.maps.donor import *
 from api.maps.country import *
 from api.maps.city import *
 from api.maps.blood_bag import *
-from flask import Blueprint
-
-blood_map = Blueprint('blood_map', __name__, url_prefix='/api')
